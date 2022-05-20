69206635bcbc7b99f809398d7d46c5ad0cb59b83
Fuad0010 <fuadft@code.edu.az>
2022-05-20 16:07:18 +0400

    feat: created empth consoleApp

---
+++ Fitness-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fitness_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
